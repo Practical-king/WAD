77bc4b27bdee15ad3ace42d5039fccbd1e115d08
Atharv Harde <atharvharde4@gmail.com>
2026-05-13 02:38:34 +0530

    first commit

---
+++ WAD 2/WAD 2B/app.js
@@ -0,0 +1,16 @@
+// npm init -y
+// npm install express
+
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Docker Container!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
